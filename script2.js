1e625d1a7413e937f11487d7ef8987cbaa159f7c
Karthika-S1305 <100103499+Karthika-S1305@users.noreply.github.com>
2024-03-31 06:36:34 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,12 @@
+<script>
+    // Example JavaScript for smooth scrolling to sections
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+  </script>
